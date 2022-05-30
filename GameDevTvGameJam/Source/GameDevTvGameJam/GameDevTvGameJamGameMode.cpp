6e48420209e6c28df432900e99a4dd65d0c47690
ZilpiONE <Kryon90@gmail.com>
2022-05-31 01:37:59 +0200

    meh

--- GameDevTvGameJam/Source/GameDevTvGameJam/GameDevTvGameJamGameMode.cpp
+++ GameDevTvGameJam/Source/GameDevTvGameJam/GameDevTvGameJamGameMode.cpp
@@ -7,7 +7,7 @@
 AGameDevTvGameJamGameMode::AGameDevTvGameJamGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/BP_Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
